15cb5d01b15c08e9434ef6698526f7a09f8b8c21
Anatoly Zelenin <anatoly@zelenin.de>
2023-11-16 14:03:31 +0100

    Consumer and Producer

--- hello_dev/consumer.py
+++ hello_dev/consumer.py
@@ -1,13 +1,21 @@
-from kafka import KafkaConsumer
-import json
+#!/usr/bin/env python3
+from confluent_kafka import Consumer
 
-# Create a Kafka Consumer
-consumer = KafkaConsumer(
-    'space-missions',
-    bootstrap_servers='localhost:9092',
-    auto_offset_reset='earliest',
-    value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+props = {'bootstrap.servers': 'localhost:9092',
+         'group.id': 'notused',
+         'enable.auto.commit': False,
+         'auto.offset.reset': 'earliest'}
+c = Consumer(props)
 
-# Receive Messages
-for message in consumer:
-    print(f"Received Message: {message.value}")
+try:
+    c.subscribe(['space_missions'])
+    while True:
+        message = c.poll(100)
+        if message is None:
+            continue
+        if message.error():
+            print("Consumer error: {}".format(message.error()))
+            continue
+        print("Received Ping: {}".format(message.value().decode('utf-8')))
+finally:
+    c.close()
--- hello_dev/producer.py
+++ hello_dev/producer.py
@@ -1,11 +1,23 @@
-from kafka import KafkaProducer
-import json
+#!/usr/bin/env python3
+from confluent_kafka import Producer
+import time
 
 # Create a new Kafka Producer
-producer = KafkaProducer(bootstrap_servers='localhost:9092',
-                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+producer = Producer({'bootstrap.servers': 'localhost:9092'})
 
-# Nachricht senden
-message = {'sender': '<your_name>', 'message': 'A new mission begins!'}
-producer.send('space-missions', value=message)
+# List of messages to be sent
+messages = [
+    "Rocket 1 launched",
+    "Rocket 2 in orbit",
+    "Rocket 3 reached its destination",
+    "Rocket 4 transmitting data",
+    "Rocket 5 returning to Earth"
+]
+
+# Sending messages
+for message in messages:
+    producer.produce('space_missions', value=message)
+    print("Produced message: " + message)
+    time.sleep(2)  # Wait for 2 seconds before sending the next message
+
 producer.flush()
